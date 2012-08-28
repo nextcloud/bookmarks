3efeef9a1dc23dc24af09760e09ba3442408de6f
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-08-28 02:04:42 +0200

    [tx-robot] updated from transifex

--- l10n/sl.php
+++ l10n/sl.php
@@ -1,12 +1,12 @@
 <?php $TRANSLATIONS = array(
 "Bookmarks" => "Zaznamki",
 "unnamed" => "neimenovano",
-"Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:" => "Povlecite to povezavo med zaznamke v vašem brskalniku in jo, ko želite ustvariti zaznamek trenutne strani, preprosto kliknite:",
+"Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:" => "Povlecite gumb med svoje zaznamke in kliknite nanj, ko želite obiskano spletno stran hitro dodati med svoje zaznamke:",
 "Read later" => "Preberi kasneje",
 "Address" => "Naslov",
 "Title" => "Ime",
 "Tags" => "Oznake",
 "Save bookmark" => "Shrani zaznamek",
 "You have no bookmarks" => "Nimate zaznamkov",
-"Bookmarklet <br />" => "Bookmarklet <br />"
+"Bookmarklet <br />" => "Vstavek za zaznamke <br />"
 );
