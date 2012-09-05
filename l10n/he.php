4325526a3cfbdf7454d2ef5e04af9b64f0662d87
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-09-05 02:06:20 +0200

    [tx-robot] updated from transifex

---
+++ l10n/he.php
@@ -0,0 +1,11 @@
+<?php $TRANSLATIONS = array(
+"Bookmarks" => "סימניות",
+"unnamed" => "ללא שם",
+"Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:" => "ניתן לגרור את הקישור הזה אל סרגל הסימניות בדפדפן שלך וללחוץ עליו כאשר מעוניינים ליצור סימניה לאתר במהירות.",
+"Read later" => "קריאה מאוחרת",
+"Address" => "כתובת",
+"Title" => "כותרת",
+"Tags" => "תגיות",
+"Save bookmark" => "שמירת סימניה",
+"You have no bookmarks" => "אין ברשותך סימניות"
+);
