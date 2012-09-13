c8d5cdd94181d9f1f5883c62364870d08a633c69
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-09-13 02:04:07 +0200

    [tx-robot] updated from transifex

--- l10n/pl.php
+++ l10n/pl.php
@@ -1,6 +1,7 @@
 <?php $TRANSLATIONS = array(
 "Bookmarks" => "Zakładki",
 "unnamed" => "bez nazwy",
+"Bookm." => "Zakładka",
 "Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:" => "Przeciągnij to do ulubionych przeglądarki i kliknij go, gdy użytkownik chce szybko dodać zakładkę strony sieci Web:",
 "Read later" => "Czytaj później",
 "Address" => "Adres",
