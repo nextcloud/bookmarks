d70df492e4d0cbabd304e468c623d805c384c5d2
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-09-17 17:01:13 +0200

    [tx-robot] updated from transifex

---
+++ l10n/zh_CN.GB2312.php
@@ -0,0 +1,13 @@
+<?php $TRANSLATIONS = array(
+"Bookmarks" => "书签",
+"unnamed" => "未命名",
+"Bookm." => "订阅它们。",
+"Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:" => "拖动此物到您的浏览器书签栏，在您想要快速订阅某网页时点击：",
+"Read later" => "稍后阅读",
+"Address" => "网址",
+"Title" => "标题",
+"Tags" => "标签",
+"Save bookmark" => "保存书签",
+"You have no bookmarks" => "您没有书签",
+"Bookmarklet <br />" => "小书签 <br />"
+);
