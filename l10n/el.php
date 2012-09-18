8dcacbcc6b123817b8196e1f6835ec9d8671a867
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-09-18 02:03:55 +0200

    [tx-robot] updated from transifex

--- l10n/el.php
+++ l10n/el.php
@@ -1,6 +1,7 @@
 <?php $TRANSLATIONS = array(
 "Bookmarks" => "Σελιδοδείκτες",
 "unnamed" => "ανώνυμο",
+"Bookm." => "Σελιδοδεικτ.",
 "Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:" => "Σύρετε αυτό στους σελιδοδείκτες του περιηγητή σας και κάντε κλικ επάνω του, όταν θέλετε να προσθέσετε σύντομα μια ιστοσελίδα ως σελιδοδείκτη:",
 "Read later" => "Ανάγνωση αργότερα",
 "Address" => "Διεύθυνση",
