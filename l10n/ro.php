d8a3a5f30321a5f581d489689f8f8de758a89eae
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-09-19 02:07:43 +0200

    [tx-robot] updated from transifex

---
+++ l10n/ro.php
@@ -0,0 +1,13 @@
+<?php $TRANSLATIONS = array(
+"Bookmarks" => "Semne de carte",
+"unnamed" => "fără denumire",
+"Bookm." => "Bookm.",
+"Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:" => "Trage acest semn în semnele de carte din navigatorul tău web când dorești să salvezi rapid un semn către o pagină web:",
+"Read later" => "Citește mai târziu",
+"Address" => "Adresă",
+"Title" => "Titlu",
+"Tags" => "Cuvinte cheie",
+"Save bookmark" => "Salvare semne de carte",
+"You have no bookmarks" => "Nu ai nici un semn de carte",
+"Bookmarklet <br />" => "Bookmarklet <br />"
+);
