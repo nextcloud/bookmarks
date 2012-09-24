65e9ca4dfa7ad218046aedcbc1728331395982a8
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-09-24 02:05:00 +0200

    [tx-robot] updated from transifex

---
+++ l10n/pt_BR.php
@@ -0,0 +1,13 @@
+<?php $TRANSLATIONS = array(
+"Bookmarks" => "Favoritos",
+"unnamed" => "sem nome",
+"Bookm." => "Fav.",
+"Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:" => "Arraste isso para os favoritos do seu navegador web e clique nele quando você quiser favoritar uma página rapidamente:",
+"Read later" => "Ler depois",
+"Address" => "Endereço",
+"Title" => "Título",
+"Tags" => "Tags",
+"Save bookmark" => "Salvar favorito",
+"You have no bookmarks" => "Você não tem favoritos",
+"Bookmarklet <br />" => "Bookmarklet <br />"
+);
