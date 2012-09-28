eaada43d9748af6f2c48600579d3a316a518ae49
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-09-28 02:08:20 +0200

    [tx-robot] updated from transifex

--- l10n/de.php
+++ l10n/de.php
@@ -2,12 +2,12 @@
 "Bookmarks" => "Lesezeichen",
 "unnamed" => "unbenannt",
 "Bookm." => "Lesez.",
-"Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:" => "Ziehen Sie dies zu Ihren Browser-Lesezeichen und klicken Sie darauf, wenn Sie eine Website schnell den Lesezeichen hinzufügen wollen.",
+"Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:" => "Ziehe dies zu Deinen Browser-Lesezeichen und klicke darauf, wenn Du eine Website schnell den Lesezeichen hinzufügen willst.",
 "Read later" => "Später lesen",
 "Address" => "Adresse",
 "Title" => "Titel",
 "Tags" => "Tags",
 "Save bookmark" => "Lesezeichen speichern",
-"You have no bookmarks" => "Sie haben keine Lesezeichen",
+"You have no bookmarks" => "Du hast keine Lesezeichen",
 "Bookmarklet <br />" => "Bookmarklet <br />"
 );
