380a883fcf21d9daba22e4f840b620e801bd04f1
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-10-12 02:08:54 +0200

    [tx-robot] updated from transifex

--- l10n/ru_RU.php
+++ l10n/ru_RU.php
@@ -1,8 +1,10 @@
 <?php $TRANSLATIONS = array(
 "Bookmarks" => "Закладки",
 "Tags" => "Теги",
+"Bookm." => "Bookm.",
 "Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:" => "Перетащите это в закладки Вашего браузера и кликните на него, когда захотите быстро добавить страницу в закладки:",
 "Read later" => "Прочитать позже",
 "Address" => "Адрес",
-"You have no bookmarks" => "У Вас нет закладок"
+"You have no bookmarks" => "У Вас нет закладок",
+"Bookmarklet <br />" => "Bookmarklet <br />"
 );
