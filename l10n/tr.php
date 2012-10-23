339d69a338615e25055f889c7fe452eaef5458f0
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-10-23 02:11:00 +0200

    [tx-robot] updated from transifex

---
+++ l10n/tr.php
@@ -0,0 +1,13 @@
+<?php $TRANSLATIONS = array(
+"Tags" => "Etiketler",
+"Close" => "Kapat",
+"Save" => "Kaydet",
+"Read later" => "Sonra oku",
+"Edit" => "Düzenle",
+"Delete" => "Sil",
+"Cancel" => "İptal",
+"Address" => "Adres",
+"List" => "Liste",
+"Hide" => "Gizle",
+"Show" => "Göster"
+);
