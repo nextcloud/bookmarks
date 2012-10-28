c781d660f3616fc8eced3854c280d8a62ddd3b67
Robin Appelman <icewind@owncloud.com>
2012-10-28 12:53:13 +0100

    Revert "[tx-robot] updated from transifex" due to merge conflicts
    This reverts commit a468d47dfb2ca740e25edfac3b11a6ea21ee1546.

--- l10n/zh_CN.php
+++ l10n/zh_CN.php
@@ -1,7 +1,6 @@
 <?php $TRANSLATIONS = array(
 "Bookmarks" => "书签",
 "unnamed" => "未命名",
-"Bookm." => "书签",
 "Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:" => "拖曳此处到您的浏览器书签处，点击可以将网页快速添加到书签中。",
 "Read later" => "稍后阅读",
 "Address" => "地址",
