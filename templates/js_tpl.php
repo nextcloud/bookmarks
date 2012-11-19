853fbcf4d2e47c77671ef42b18124e22212ff333
raghunayyar <me@iraghu.com>
2012-11-20 01:16:52 +0530

    Relative lengths of Bookmark Elements.
    Tried my best that no overlapping happs for larger widths, removed the
    second edit button too.

--- templates/js_tpl.php
+++ templates/js_tpl.php
@@ -10,10 +10,6 @@
 					<a href="<%= encodeEntities(url) %>" target="_blank" class="bookmark_link">
 						<%= encodeEntities(title == '' ? url : title ) %>
 					</a>
-                                    	<span class="bookmark_edit">
-						<img class="svg" src="<?php echo OCP\image_path("", "actions/rename.svg");?>"
-							title="<?php echo $l->t('Edit');?>">
-					</span>
                                     	<span class="bookmark_desc"><%= encodeEntities(description)%> </span>
                                         <span class="bookmark_date"><%= formatDate(added_date) %></span>
 				</p>
