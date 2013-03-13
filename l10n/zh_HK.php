46758757e78701ce75f669465364135d59f2d696
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-03-14 00:04:34 +0100

    [tx-robot] updated from transifex

---
+++ l10n/zh_HK.php
@@ -0,0 +1,11 @@
+<?php $TRANSLATIONS = array(
+"Bookmarks" => "收藏",
+"Save" => "儲存",
+"Delete" => "刪除",
+"Edit" => "編輯",
+"Cancel" => "取消",
+"Address" => "地址",
+"Add" => "加入",
+"Settings" => "設定",
+"Import" => "導入"
+);
