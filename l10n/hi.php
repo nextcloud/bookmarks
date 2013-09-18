1dc191efd069e37f358b8b782916ada5d37c0464
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-09-18 11:49:32 -0400

    [tx-robot] updated from transifex

--- l10n/hi.php
+++ l10n/hi.php
@@ -1,4 +1,6 @@
 <?php $TRANSLATIONS = array(
+"Warning" => "चेतावनी ",
+"Close" => "बंद करें ",
 "Save" => "सहेजें",
 "Add" => "डाले",
 "Settings" => "सेटिंग्स"
---
+++ l10n/pa.php
@@ -0,0 +1,7 @@
+<?php $TRANSLATIONS = array(
+"Bookmarks" => "ਬੁੱਕਮਾਰ",
+"Warning" => "ਚੇਤਾਵਨੀ",
+"Delete" => "ਹਟਾਓ",
+"Cancel" => "ਰੱਦ ਕਰੋ",
+"Settings" => "ਸੈਟਿੰਗ"
+);
