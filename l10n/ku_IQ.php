c0c26d4b1c93586b5c53267f545843e61452d3ee
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-01-20 02:10:54 -0500

    [tx-robot] updated from transifex

--- l10n/ku_IQ.php
+++ l10n/ku_IQ.php
@@ -6,6 +6,7 @@
 "Close" => "داخستن",
 "Save" => "پاشکه‌وتکردن",
 "Edit" => "دەسکاریکردن",
+"Cancel" => "لابردن",
 "Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:" => "رابکێشه‌ ئه‌مه بۆ دڵخوازه‌کان له‌ وێبگه‌ڕه‌که‌ت وه‌ کرته‌ بکه‌ لێی، کاتێك ده‌ته‌وێت ماڵپه‌ڕێك دڵخواز بکه‌ی به‌خێرای:",
 "Address" => "ناونیشان",
 "Add" => "زیادکردن",
