fee4630e506b4fd1a24935116ca27d0fb0c322fc
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-06-06 02:10:07 -0400

    [tx-robot] updated from transifex

--- l10n/lv.php
+++ l10n/lv.php
@@ -21,6 +21,7 @@
 "Edit" => "Rediģēt",
 "Cancel" => "Atcelt",
 "Drag this to your browser bookmarks and click it, when you want to bookmark a webpage quickly:" => "Ievelc šo savās pārlūka grāmatzīmēs un spied uz tā, kad vēlies ātri pievienot tīmekļa lapu grāmatzīmēm:",
+"Add to ownCloud" => "Pievienot ownCloud",
 "Address" => "Adrese",
 "Add" => "Pievienot",
 "Related Tags" => "Saistītās birkas",
