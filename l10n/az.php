d4712bd90c85ec719cd36d0fda97ee97d51ddca9
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-17 02:10:22 -0400

    [tx-robot] updated from transifex

---
+++ l10n/az.php
@@ -0,0 +1,13 @@
+<?php $TRANSLATIONS = array(
+"No file provided for import" => "Götürmək üçün heç bir fayl təqdim edilməyib",
+"Unsupported file type for import" => "Fayl tipi götürülmə üçün dəstəklənmir",
+"Bookmarks" => "Sayt qeydiyyatları",
+"Tags" => "Işarələr",
+"Filter by tag" => "Əsas-a görə süzgəc",
+"Close" => "Bağla",
+"Save" => "Saxlamaq",
+"Cancel" => "Dayandır",
+"Settings" => "Quraşdırmalar",
+"Export" => "Çıxarış",
+"Import" => "Əlavə et"
+);
