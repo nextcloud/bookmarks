2bf97ef0da637dfaa8dbf3502d31c9b5c4b707ba
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-11-27 02:10:23 -0500

    [tx-robot] updated from transifex

--- l10n/da.php
+++ l10n/da.php
@@ -1,19 +1,17 @@
 <?php
 $TRANSLATIONS = array(
+"Bookmarks" => "Bogmærker",
+"Bookm." => "Bogm.",
 "No file provided for import" => "Der blev ikke angivet en fil til import",
 "Unsupported file type for import" => "Filtypen understøttes ikke for import",
-"Bookmarks" => "Bogmærker",
-"Tags" => "Mærker",
-"Filter by tag" => "Filtrer efter tag",
-"Edit bookmark" => "Rediger bogmærker",
-"Are you sure you want to remove this tag from every entry?" => "Er du sikker på at du vil fjerne dette flag fra alle poster?",
+"Filter by tag" => "Filtrér efter mærke",
 "Warning" => "Advarsel",
+"Tags" => "Mærker",
+"Are you sure you want to remove this tag from every entry?" => "Er du sikker på at du vil fjerne dette flag fra alle poster?",
 "Import error" => "Fejl ved import",
-"Import completed successfully." => "Importer fuldført.",
+"Import completed successfully." => "Importen blev fuldført.",
 "Uploading..." => "Uploader...",
-"Bookm." => "Bogm.",
 "Add a bookmark" => "Tilføj bogmærke",
-"Close" => "Luk",
 "The title of the page" => "Sidens titel",
 "The address of the page" => "Sidens adresse",
 "Description of the page" => "Sidens beskrivelse",
@@ -25,13 +23,10 @@ $TRANSLATIONS = array(
 "Add to ownCloud" => "Tilføj til ownCloud",
 "Address" => "Adresse",
 "Add" => "Tilføj",
-"Related Tags" => "Relaterede Tags",
-"Settings" => "Indstillinger",
 "You have no bookmarks" => "Du har ingen bogmærker",
-"You can also try to import a bookmark file" => "Du kan også prøve at importere en bogmærke-fil",
 "Bookmarklet" => "Bookmarklet",
 "Export & Import" => "Eksport & import",
-"Export" => "Exporter",
-"Import" => "Importer"
+"Export" => "Eksportér",
+"Import" => "Importér"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
