627219b39020aa07fe4d15af52da65960cffd7ce
Elie Khoury <elie@el-khoury.net>
2015-12-15 19:42:51 +0100

    extend the rest api to support adding / updating and deleting of bookmarks

--- controller/rest/publiccontroller.php
+++ controller/rest/publiccontroller.php
@@ -71,6 +71,117 @@ class PublicController extends ApiController {
 		return new JSONResponse($output);
 	}
 
+
+    /**
+     * @CORS
+     * @NoAdminRequired
+     * @NoCSRFRequired
+     * @PublicPage
+     */
+    public function returnAddAsJson($user, $password, $url = "", $tags = array(), $title = "", $description = "") {
+
+        if ($user == null || $this->userManager->userExists($user) == false) {
+            return $this->newJsonErrorMessage("User could not be identified");
+        }
+
+        if ($tags[0] == "") {
+            $tags = array();
+        }
+
+        if (!$this->userManager->checkPassword($user, $password)) {
+
+            $msg = 'REST API accessed with wrong password';
+            \OCP\Util::writeLog('bookmarks', $msg, \OCP\Util::WARN);
+
+            return $this->newJsonErrorMessage("Wrong password for user " . $user);
+        }
+
+        $output = Bookmarks::addBookmark($user, $this->db, $url, $title, $tags, $description, false);
+
+        if (count($output) == 0) {
+            $output["status"] = 'error';
+            $output["message"] = "No results from this query";
+            return new JSONResponse($output);
+        }
+
+        $output = Bookmarks::findUniqueBookmark($output,$user,$this->db);
+
+        return new JSONResponse($output);
+    }
+
+    /**
+     * @CORS
+     * @NoAdminRequired
+     * @NoCSRFRequired
+     * @PublicPage
+     */
+    public function returnUpdateAsJson($user, $password, $id, $url = "", $tags = array(), $title = "", $description = "") {
+
+        if ($user == null || $this->userManager->userExists($user) == false) {
+            return $this->newJsonErrorMessage("User could not be identified");
+        }
+
+        if ($tags[0] == "") {
+            $tags = array();
+        }
+
+        if (!$this->userManager->checkPassword($user, $password)) {
+
+            $msg = 'REST API accessed with wrong password';
+            \OCP\Util::writeLog('bookmarks', $msg, \OCP\Util::WARN);
+
+            return $this->newJsonErrorMessage("Wrong password for user " . $user);
+        }
+
+
+        $output = Bookmarks::editBookmark($user, $this->db, $id, $url, $title, $tags, $description, false);
+
+        if (count($output) == 0) {
+            $output["status"] = 'error';
+            $output["message"] = "No results from this query";
+            return new JSONResponse($output);
+        }
+
+        $output = Bookmarks::findUniqueBookmark($output,$user,$this->db);
+
+        return new JSONResponse($output);
+    }
+
+    /**
+     * @CORS
+     * @NoAdminRequired
+     * @NoCSRFRequired
+     * @PublicPage
+     */
+    public function returnDeleteAsJson($user, $password, $id) {
+        if ($user == null || $this->userManager->userExists($user) == false) {
+            return $this->newJsonErrorMessage("User could not be identified");
+        }
+
+        if (!$this->userManager->checkPassword($user, $password)) {
+
+            $msg = 'REST API accessed with wrong password';
+            \OCP\Util::writeLog('bookmarks', $msg, \OCP\Util::WARN);
+
+            return $this->newJsonErrorMessage("Wrong password for user " . $user);
+        }
+
+
+        $output = Bookmarks::deleteUrl($user, $this->db, $id);
+
+        if (!$output) {
+            $output = array();
+            $output["status"] = 'error';
+            $output["message"] = "Cannot delete bookmark";
+            return new JSONResponse($output);
+        }else{
+            $output = array();
+            $output["status"] = 'success';
+            $output["message"] = "Bookmark deleted";
+            return new JSONResponse($output);
+        }
+    }
+
 	public function newJsonErrorMessage($message) {
 		$output = array();
 		$output["status"] = 'error';
