90d1845966c67682dbbf589fd26b01f8fa8e45b3
Eli Khoury <elie@el-khoury.net>
2015-12-17 09:53:05 +0100

    kept the public json method and added w new one for backward compatibility

--- controller/rest/publiccontroller.php
+++ controller/rest/publiccontroller.php
@@ -9,47 +9,71 @@ use \OCP\AppFramework\Http\JSONResponse;
 use \OC\User\Manager;
 use OCA\Bookmarks\Controller\Lib\Bookmarks;
 
-class PublicController extends ApiController {
+class PublicController extends ApiController
+{
 
-	private $db;
-	private $userManager;
+    private $db;
+    private $userManager;
 
-	public function __construct($appName, IRequest $request, IDb $db, Manager $userManager) {
-		parent::__construct(
-				$appName, $request);
+    public function __construct($appName, IRequest $request, IDb $db, Manager $userManager)
+    {
+        parent::__construct(
+            $appName, $request);
 
-		$this->db = $db;
-		$this->userManager = $userManager;
-	}
+        $this->db = $db;
+        $this->userManager = $userManager;
+    }
+
 
     /**
-	 * @CORS
-	 * @NoAdminRequired
-	 * @NoCSRFRequired
-	 */
-	public function returnAsJson($user, $tags = array(), $conjunction = "or", $select = null, $sortby = "") {
+     * @CORS
+     * @NoAdminRequired
+     * @NoCSRFRequired
+     * @PublicPage
+     */
+    public function returnAsJson($user, $password = null, $tags = array(), $conjunction = "or", $select = null, $sortby = "")
+    {
 
-		if ($tags != null && $tags[0] == "") {
-			$tags = array();
-		}
+        if ($user == null || $this->userManager->userExists($user) == false) {
+            return $this->newJsonErrorMessage("User could not be identified");
+        }
 
-		$attributesToSelect = array('url', 'title');
+        if ($tags[0] == "") {
+            $tags = array();
+        }
 
-		if ($select != null) {
-			$attributesToSelect = array_merge($attributesToSelect, $select);
-			$attributesToSelect = array_unique($attributesToSelect);
-		}
+        $public = true;
 
-		$output = Bookmarks::findBookmarks($user, $this->db, 0, $sortby, $tags, true, -1, false, $attributesToSelect, $conjunction);
+        if ($password != null) {
+            $public = false;
+        }
 
-		if (count($output) == 0) {
-			$output["status"] = 'error';
-			$output["message"] = "No results from this query";
-			return new JSONResponse($output);
-		}
 
-		return new JSONResponse($output);
-	}
+        if (!$public && !$this->userManager->checkPassword($user, $password)) {
+
+            $msg = 'REST API accessed with wrong password';
+            \OCP\Util::writeLog('bookmarks', $msg, \OCP\Util::WARN);
+
+            return $this->newJsonErrorMessage("Wrong password for user " . $user);
+        }
+
+        $attributesToSelect = array('url', 'title');
+
+        if ($select != null) {
+            $attributesToSelect = array_merge($attributesToSelect, $select);
+            $attributesToSelect = array_unique($attributesToSelect);
+        }
+
+        $output = Bookmarks::findBookmarks($user, $this->db, 0, $sortby, $tags, true, -1, $public, $attributesToSelect, $conjunction);
+
+        if (count($output) == 0) {
+            $output["status"] = 'error';
+            $output["message"] = "No results from this query";
+            return new JSONResponse($output);
+        }
+
+        return new JSONResponse($output);
+    }
 
 
     /**
@@ -57,7 +81,39 @@ class PublicController extends ApiController {
      * @NoAdminRequired
      * @NoCSRFRequired
      */
-    public function returnAddAsJson($user, $url = "", $tags = array(), $title = "", $description = "") {
+    public function returnPrivateAsJson($user, $tags = array(), $conjunction = "or", $select = null, $sortby = "")
+    {
+
+        if ($tags != null && $tags[0] == "") {
+            $tags = array();
+        }
+
+        $attributesToSelect = array('url', 'title');
+
+        if ($select != null) {
+            $attributesToSelect = array_merge($attributesToSelect, $select);
+            $attributesToSelect = array_unique($attributesToSelect);
+        }
+
+        $output = Bookmarks::findBookmarks($user, $this->db, 0, $sortby, $tags, true, -1, false, $attributesToSelect, $conjunction);
+
+        if (count($output) == 0) {
+            $output["status"] = 'error';
+            $output["message"] = "No results from this query";
+            return new JSONResponse($output);
+        }
+
+        return new JSONResponse($output);
+    }
+
+
+    /**
+     * @CORS
+     * @NoAdminRequired
+     * @NoCSRFRequired
+     */
+    public function returnAddAsJson($user, $url = "", $tags = array(), $title = "", $description = "")
+    {
 
         if ($tags[0] == "") {
             $tags = array();
@@ -71,7 +127,7 @@ class PublicController extends ApiController {
             return new JSONResponse($output);
         }
 
-        $output = Bookmarks::findUniqueBookmark($output,$user,$this->db);
+        $output = Bookmarks::findUniqueBookmark($output, $user, $this->db);
 
         return new JSONResponse($output);
     }
@@ -81,7 +137,8 @@ class PublicController extends ApiController {
      * @NoAdminRequired
      * @NoCSRFRequired
      */
-    public function returnUpdateAsJson($user, $id, $url = "", $tags = array(), $title = "", $description = "") {
+    public function returnUpdateAsJson($user, $id, $url = "", $tags = array(), $title = "", $description = "")
+    {
 
         if ($tags[0] == "") {
             $tags = array();
@@ -95,7 +152,7 @@ class PublicController extends ApiController {
             return new JSONResponse($output);
         }
 
-        $output = Bookmarks::findUniqueBookmark($output,$user,$this->db);
+        $output = Bookmarks::findUniqueBookmark($output, $user, $this->db);
 
         return new JSONResponse($output);
     }
@@ -105,7 +162,8 @@ class PublicController extends ApiController {
      * @NoAdminRequired
      * @NoCSRFRequired
      */
-    public function returnDeleteAsJson($user, $id) {
+    public function returnDeleteAsJson($user, $id)
+    {
         $output = Bookmarks::deleteUrl($user, $this->db, $id);
 
         if (!$output) {
@@ -113,7 +171,7 @@ class PublicController extends ApiController {
             $output["status"] = 'error';
             $output["message"] = "Cannot delete bookmark";
             return new JSONResponse($output);
-        }else{
+        } else {
             $output = array();
             $output["status"] = 'success';
             $output["message"] = "Bookmark deleted";
@@ -126,7 +184,8 @@ class PublicController extends ApiController {
      * @NoAdminRequired
      * @NoCSRFRequired
      */
-    public function returnClickBookmarkAsJson($user, $url) {
+    public function returnClickBookmarkAsJson($user, $url)
+    {
 
         // Check if it is a valid URL
         if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
@@ -148,11 +207,12 @@ class PublicController extends ApiController {
         return new JSONResponse($output);
     }
 
-	public function newJsonErrorMessage($message) {
-		$output = array();
-		$output["status"] = 'error';
-		$output["message"] = $message;
-		return new JSONResponse($output);
-	}
+    public function newJsonErrorMessage($message)
+    {
+        $output = array();
+        $output["status"] = 'error';
+        $output["message"] = $message;
+        return new JSONResponse($output);
+    }
 
 }
--- tests/publiccontroller_test.php
+++ tests/publiccontroller_test.php
@@ -7,51 +7,51 @@ use OCA\Bookmarks\Controller\Lib\Bookmarks;
 
 class Test_PublicController_Bookmarks extends PHPUnit_Framework_TestCase {
 
-	private $userid;
-	private $request;
-	private $db;
-	private $userManager;
-	private $publicController;
+    private $userid;
+    private $request;
+    private $db;
+    private $userManager;
+    private $publicController;
 
-	protected function setUp() {
-		$this->userid = "testuser";
-		$this->request = \OC::$server->getRequest();
-		$this->db = \OC::$server->getDb();
-		$this->userManager = \OC::$server->getUserManager();
-		$this->publicController = new PublicController("bookmarks", $this->request, $this->db, $this->userManager);
-	}
+    protected function setUp() {
+        $this->userid = "testuser";
+        $this->request = \OC::$server->getRequest();
+        $this->db = \OC::$server->getDb();
+        $this->userManager = \OC::$server->getUserManager();
+        $this->publicController = new PublicController("bookmarks", $this->request, $this->db, $this->userManager);
+    }
 
-	function testPublicQueryNoUser() {
-		$output = $this->publicController->returnAsJson(null, null);
-		$data = $output->getData();
-		$status = $data['status'];
-		$this->assertEquals($status, 'error');
-	}
+    function testPublicQueryNoUser() {
+        $output = $this->publicController->returnAsJson(null, "apassword", null);
+        $data = $output->getData();
+        $status = $data['status'];
+        $this->assertEquals($status, 'error');
+    }
 
-	function testPublicQueryWrongUser() {
-		$output = $this->publicController->returnAsJson("cqc43dr4rx3x4xatr4", null);
-		$data = $output->getData();
-		$status = $data['status'];
-		$this->assertEquals($status, 'error');
-	}
+    function testPublicQueryWrongUser() {
+        $output = $this->publicController->returnAsJson("cqc43dr4rx3x4xatr4", "apassword", null);
+        $data = $output->getData();
+        $status = $data['status'];
+        $this->assertEquals($status, 'error');
+    }
 
-	function testPublicQuery() {
+    function testPublicQuery() {
 
-		Bookmarks::addBookmark($this->userid, $this->db, "http://www.golem.de", "Golem", array("four"), "NoTag", false);
-		Bookmarks::addBookmark($this->userid, $this->db, "http://www.9gag.com", "9gag", array("two", "three"), "Tag", false);
+        Bookmarks::addBookmark($this->userid, $this->db, "http://www.golem.de", "Golem", array("four"), "PublicNoTag", true);
+        Bookmarks::addBookmark($this->userid, $this->db, "http://www.9gag.com", "9gag", array("two", "three"), "PublicTag", true);
 
-		$output = $this->publicController->returnAsJson($this->userid);
-		$data = $output->getData();
-		$this->assertEquals(2, count($data));
-		
-		$this->cleanDB();
-	}
+        $output = $this->publicController->returnAsJson($this->userid);
+        $data = $output->getData();
+        $this->assertEquals(2, count($data));
 
-	function cleanDB() {
-		$query1 = OC_DB::prepare('DELETE FROM *PREFIX*bookmarks');
-		$query1->execute();
-		$query2 = OC_DB::prepare('DELETE FROM *PREFIX*bookmarks_tags');
-		$query2->execute();
-	}
+        $this->cleanDB();
+    }
+
+    function cleanDB() {
+        $query1 = OC_DB::prepare('DELETE FROM *PREFIX*bookmarks');
+        $query1->execute();
+        $query2 = OC_DB::prepare('DELETE FROM *PREFIX*bookmarks_tags');
+        $query2->execute();
+    }
 
 }
