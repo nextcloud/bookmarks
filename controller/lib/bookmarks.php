3492d46f7aacfff1e9d0c0591502719108a2c77b
Marcel Klehr <mklehr@gmx.net>
2017-05-16 11:27:57 +0200

    Fix lib#deleteTag: Fix copy-pasta leftover

--- controller/lib/bookmarks.php
+++ controller/lib/bookmarks.php
@@ -372,9 +372,8 @@ class Bookmarks {
 			$qb = $this->db->getQueryBuilder();
 			$qb
 				->delete('bookmarks_tags', 'tgs')
-				->innerJoin('tgs', 'bookmarks', 'bm', $qb->expr()->eq('tgs.bookmark_id', 'bm.id'))
 				->where($qb->expr()->eq('tgs.tag', $qb->createNamedParameter($old)))
-				->andWhere($qb->expr()->in('bm.user_id', $qb->createNamedParameter($bookmarks)));
+				->andWhere($qb->expr()->in('bookmark_id', $qb->createNamedParameter($bookmarks)));
 			return $qb->execute();
 		}
 		return true;
