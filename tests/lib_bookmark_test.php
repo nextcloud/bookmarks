2aec8f3df7bc620614043a62c9dcdf6d6132d2af
Marcel Klehr <mklehr@gmx.net>
2017-05-16 18:27:42 +0200

    Fix deleteTag test: Fix assertion

--- tests/lib_bookmark_test.php
+++ tests/lib_bookmark_test.php
@@ -157,7 +157,7 @@ class Test_LibBookmarks_Bookmarks extends TestCase {
 		$this->assertFalse(in_array(['tag' => 'one', 'nbr' => 2], $firstUserTags));
 		$this->assertTrue(in_array(['tag' => 'two', 'nbr' => 2], $firstUserTags));
 		$this->assertTrue(in_array(['tag' => 'three', 'nbr' => 1], $firstUserTags));
-		$this->assertEquals(count($firstUserTags), 3);
+		$this->assertEquals(count($firstUserTags), 2);
 		$secondUserTags = $this->libBookmarks->findTags($secondUser);
 		$this->assertTrue(in_array(['tag' => 'one', 'nbr' => 2], $secondUserTags));
 		$this->assertTrue(in_array(['tag' => 'two', 'nbr' => 2], $secondUserTags));
