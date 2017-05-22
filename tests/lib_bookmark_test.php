769b756c3c62a9e9590680c6bbef6dd155e89288
Marcel Klehr <mklehr@gmx.net>
2017-05-22 23:03:03 +0200

    Fix desparate movFix desparate

--- tests/lib_bookmark_test.php
+++ tests/lib_bookmark_test.php
@@ -158,8 +158,8 @@ class Test_LibBookmarks_Bookmarks extends TestCase {
 		$this->assertTrue(in_array(['tag' => 'two', 'nbr' => 2], $firstUserTags));
 		$this->assertTrue(in_array(['tag' => 'four', 'nbr' => 1], $firstUserTags));
 		$this->assertTrue(in_array(['tag' => 'three', 'nbr' => 1], $firstUserTags));
-		$this->assertEquals(count($firstUserTags), 3);
 		var_dump($firstUserTags);
+		$this->assertEquals(count($firstUserTags), 3);
 		$secondUserTags = $this->libBookmarks->findTags($secondUser);
 		$this->assertTrue(in_array(['tag' => 'one', 'nbr' => 2], $secondUserTags));
 		$this->assertTrue(in_array(['tag' => 'two', 'nbr' => 2], $secondUserTags));
