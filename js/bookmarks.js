60aa47030669b0d9719beab07bb52ce42fad39a9
Marcel Klehr <mklehr@gmx.net>
2018-01-13 20:25:55 +0100

    UI: Use official app settings styles

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -283,16 +283,17 @@ var TagsNavigationTagView = Marionette.View.extend({
 
 var SettingsView = Marionette.View.extend({
   className: 'settings'
-, template: _.template('<a href="#" class="open"><img src="../../../core/img/actions/settings.svg?v=1" /> Settings</a><div class="settings-content"><h3>Import & Export</h3><form class="import-form" action="bookmark/import" method="post" target="upload_iframe" enctype="multipart/form-data" encoding="multipart/form-data"><input type="file" class="import" name="bm_import" size="5" /><input type="hidden" name="requesttoken" value="'+oc_requesttoken+'" /><button class="import-facade">Import</button></form><iframe class="upload" name="upload_iframe" id="upload_iframe"></iframe><button class="export">Export</button><div class="import-status"></div></div>')
+, id: 'app-settings'
+, template: _.template('<div id="app-settings-header"><button class="settings-button">Settings</a></div><div id="app-settings-content"><form class="import-form" action="bookmark/import" method="post" target="upload_iframe" enctype="multipart/form-data" encoding="multipart/form-data"><input type="file" class="import" name="bm_import" size="5" /><input type="hidden" name="requesttoken" value="'+oc_requesttoken+'" /><button class="import-facade">Import</button></form><iframe class="upload" name="upload_iframe" id="upload_iframe"></iframe><button class="export">Export</button><div class="import-status"></div></div>')
 , ui: {
-    'content': '.settings-content'
+    'content': '#app-settings-content'
   , 'import': '.import'
   , 'form': '.import-form'
   , 'iframe': '.upload'
   , 'status': '.import-status'
   }
 , events: {
-    'click .open': 'open'
+    'click .settings-button': 'open'
   , 'click .import-facade': 'importTrigger'
   , 'change @ui.import': 'importSubmit'
   , 'load @ui.iframe': 'importResult'
