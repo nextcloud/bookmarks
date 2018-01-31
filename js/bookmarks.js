85aa8ec484307ab1d5ab7a5688f39857ba6cdee2
Marcel Klehr <mklehr@gmx.net>
2018-01-31 17:00:51 +0100

    Bulk tagging

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -450,11 +450,11 @@ var MobileNavView = Marionette.View.extend({
 
 var BulkActionsView = Marionette.View.extend({
   className: 'bulk-actions'
-, template: _.template('<button class="delete"><span class="icon-delete"></span></button><div class="selection-tools"><button class="select-all"><span class="icon-checkmark"></span> Select all visible</button><div class="close"><span class="icon-close"></span></div></div>')
+, template: _.template('<div class="selection-tools"><button class="select-all"><span class="icon-checkmark"></span> Select all visible</button><div class="close"><span class="icon-close"></span></div></div><button class="delete"><span class="icon-delete"></span><span>Delete</span></button><div class="tags"><input type="text" /></div></div>')
 , events: {
     'click .delete': 'delete'
   , 'click .select-all': 'selectAll'
-  , 'click .close': 'abort'
+  , 'click .selection-tools .close': 'abort'
   }
 , initialize: function(opts) {
     this.all = opts.all
@@ -462,15 +462,32 @@ var BulkActionsView = Marionette.View.extend({
     this.listenTo(this.selected, 'remove', this.onReduceSelection)
     this.listenTo(this.selected, 'add', this.onExtendSelection)
   }
+, onRender: function() {
+    this.rendering = true
+    this.$('.tags input')
+    .val(_.intersection.apply(_, this.selected.pluck('tags')).join(','))
+    .tagit({
+      allowSpaces: true,
+      availableTags: app.tags.pluck('name'),
+      placeholderText: t('bookmarks', 'Enter tags'),
+      onTagRemoved: this.onTagRemoved.bind(this),
+      onTagAdded: this.onTagAdded.bind(this),
+      onTagFinishRemoved: function() {},
+      onTagClicked: function(){}
+    })
+    this.rendering = false
+  }
 , onReduceSelection: function() {
     if (this.selected.length == 0) {
-	  this.$el.removeClass('active')
-	}
+	    this.$el.removeClass('active')
+    }
+    this.render()
   }
 , onExtendSelection: function() {
     if (this.selected.length == 1) {
-	  this.$el.addClass('active')
-	}
+      this.$el.addClass('active')
+    }
+    this.render()
   }
 , delete: function() {
     var that = this
@@ -482,6 +499,27 @@ var BulkActionsView = Marionette.View.extend({
         }
       })
     })
+  }
+, onTagAdded: function(e, el) {
+    if (this.rendering) return
+    var tagName = $('.tagit-label', el).text()
+    this.selected.forEach(function(model) {
+      var tags = model.get('tags')
+      model.set('tags', _.union(tags, [tagName]))
+      model.save()
+    }) 
+  }
+, onTagRemoved: function(e, el) {
+    if (this.rendering) return
+    var tagName = $('.tagit-label', el).text()
+    this.selected.forEach(function(model) {
+      var tags = model.get('tags')
+      model.set('tags', _.without(tags, tagName))
+      model.save()
+    }) 
+  }
+, onChildViewSelectionChange: function() {
+    alert('boojah')
   }
 , selectAll: function() {
     this.all.forEach(function(model) {
@@ -671,7 +709,6 @@ var BookmarkDetailView = Marionette.View.extend({
     this.close() 
   }
 })
-
 
 
 var TagsManagementView = Marionette.CollectionView.extend({
