a3c83b519585ca1beddffdef0a38c40e09dc05aa
Marcel Klehr <mklehr@gmx.net>
2018-02-20 19:49:02 +0100

    Fix app.php

--- appinfo/app.php
+++ appinfo/app.php
@@ -14,7 +14,7 @@
 namespace OCA\Bookmarks\AppInfo;
 
 if ((@include_once __DIR__ . '/../vendor/autoload.php')===false) {
-  throw new Exception('Cannot include autoload. Did you run install dependencies using composer?');
+  throw new \Exception('Cannot include autoload. Did you run install dependencies using composer?');
 }
 
 $navigationEntry = function () {
