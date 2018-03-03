1cf8b6865c2c065ce402214e59d986c6203fb874
Marcel Klehr <mklehr@gmx.net>
2018-03-03 16:38:54 +0100

    Display spinner if the user has to wait

--- js/views/EmptyBookmarks.js
+++ js/views/EmptyBookmarks.js
@@ -1,11 +1,23 @@
 import _ from 'underscore';
 import Backbone from 'backbone';
-import templateString from '../templates/EmptyBookmarks.html';
+import templateStringEmpty from '../templates/EmptyBookmarks.html';
+import templateStringLoading from '../templates/LoadingBookmarks.html';
 
 const Marionette = Backbone.Marionette;
 const Radio = Backbone.Radio;
 
 export default Marionette.View.extend({
-	template: _.template(templateString),
-	className: 'bookmarks-empty'
+	getTemplate: function() {
+		if (this.app.bookmarks.loadingState.get('fetching')) {
+			return _.template(templateStringLoading);
+		
+		} else {
+			return _.template(templateStringEmpty);
+		}
+	},
+	className: 'bookmarks-empty',
+	initialize: function(options) {
+		this.app = options.app;
+		this.listenTo(this.app.bookmarks.loadingState, 'change:fetching', this.render);
+	}
 });
