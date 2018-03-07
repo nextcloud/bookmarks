5bfd562a1573d53725367ef094cbf7ebda8fa4b9
Marcel Klehr <mklehr@gmx.net>
2018-03-08 00:05:34 +0100

    Make bookmarklet work

--- templates/addBookmarklet.php
+++ templates/addBookmarklet.php
@@ -1,64 +1,54 @@
 <?php
-OCP\Util::addscript('bookmarks', '3rdparty/tag-it');
-OCP\Util::addscript('bookmarks', 'bookmarklet');
-OCP\Util::addStyle('bookmarks', 'bookmarks');
-OCP\Util::addStyle('bookmarks', '3rdparty/jquery.tagit');
+OCP\Util::addscript('bookmarks', 'dist/bookmarklet.bundle');
+OCP\Util::addStyle('bookmarks', 'bookmarklet');
+style('bookmarks', 'select2');
 
 $bookmarkExists = $_['bookmarkExists'];
 ?>
 <div id="bookmarklet_form">
     <form class="addBm" action="">
-		<script type="text/javascript" src="tag"></script>
+		<div id="add_form_loading" style=""><span class="icon-loading"></span></div>
+			<h1><?php p($l->t('Add a bookmark')); ?></h1>
 
-		<h1 style="display: block; float: left"><?php p($l->t('Add a bookmark')); ?></h1>
-		<span style="display: inline; float: right"><div id="add_form_loading" style="margin: 3px;"><img src="<?php print_unescaped(image_path("bookmarks", "loading.gif")); ?>"> </div></span>
-
-		<div style="color: red; clear: both; visibility: <?php
-		if ($bookmarkExists === false) {
-			print_unescaped('hidden');
-		}
-		?>">
-			<strong>
-				<?php p($l->t('This URL is already bookmarked! Overwrite?')); ?>
-			</strong>
-		</div>
-
-        <fieldset class="bm_desc">
-            <ul>
-                <li>
-					<?php if($bookmarkExists !== false) { ?>
+			<div class="bookmark-exists" style="display: <?php
+			if ($bookmarkExists === false) {
+				print_unescaped('none');
+			}
+			?>">
+					<?php p($l->t('This URL is already bookmarked! Overwrite?')); ?>
+			</div>
+			<ul class="addBm">
+					<li>
+						<?php if($bookmarkExists !== false) { ?>
 						<input id="bookmarkID" type="hidden" class="hidden" value="<?php p($bookmarkExists); ?>" />
-					<?php } ?>
-                    <input id="title" type="text" name="title" class="title" value="<?php p($_['title']); ?>"
-                           placeholder="<?php p($l->t('The title of the page')); ?>" />
-                </li>
+						<?php } ?>
+						<input id="title" type="text" name="title" class="title" value="<?php p($_['title']); ?>"
+										 placeholder="<?php p($l->t('The title of the page')); ?>" />
+					</li>
 
-                <li>
-                    <input id="url" type="text" name="url" class="url_input" value="<?php p($_['url']); ?>"
-                           placeholder="<?php p($l->t('The address of the page')); ?>" />
-                </li>
+					<li>
+						<input id="url" type="text" name="url" class="url_input" value="<?php p($_['url']); ?>"
+										 placeholder="<?php p($l->t('The address of the page')); ?>" />
+					</li>
 
-                <li>
-                    <ul id="tags" class="tags" >
-                        <?php foreach ($_['tags'] as $tag): ?>
-                            <li><?php p($tag); ?></li>
-                        <?php endforeach; ?>
-                    </ul>
-                </li>
+					<li>
+						<ul id="tags" class="tags" >
+							<?php foreach ($_['tags'] as $tag): ?>
+									<li><?php p($tag); ?></li>
+							<?php endforeach; ?>
+						</ul>
+					</li>
 
-                <li>
-                    <textarea id="description" name="description" class="desc"
-                              placeholder="<?php p($l->t('Description of the page')); ?>"><?php p($_['description']); ?></textarea>
-                </li>
+					<li>
+						<textarea id="description" name="description" class="desc"
+										placeholder="<?php p($l->t('Description of the page')); ?>"><?php p($_['description']); ?></textarea>
+					</li>
 
-                <li>
-                    <input type="submit" class="submit" value="<?php p($l->t("Save")); ?>" />
-                    <input type="hidden" class="record_id" value="" name="record_id" />
-                    <input type="hidden" name="requesttoken" value="<?php p($_['requesttoken']) ?>">
-                </li>
-
-            </ul>
-
-        </fieldset>
+					<li>
+						<input type="submit" class="submit" value="<?php p($l->t("Save")); ?>" />
+						<input type="hidden" class="record_id" value="" name="record_id" />
+						<input type="hidden" name="requesttoken" value="<?php p($_['requesttoken']) ?>">
+					</li>
+			</ul>
     </form>
 </div>
