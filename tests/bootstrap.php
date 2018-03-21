5fce47772c9ef3e8bc0c9992e926fcbda51e67ce
Marcel Klehr <mklehr@gmx.net>
2018-03-21 20:17:54 +0100

    Try something

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ define('PHPUNIT_RUN', 1);
 require_once __DIR__ . '/../../../lib/base.php';
 
 if (!class_exists('PHPUnit_Framework_TestCase')) {
-	require_once('PHPUnit/Autoload.php');
+	//require_once('PHPUnit/Autoload.php');
 }
 
 \OC_App::loadApp('bookmarks');
