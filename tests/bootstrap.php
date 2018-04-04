b3a6f79d956a301a3328d9b5d8c1993077bfe002
Arthur Schiwon <blizzz@arthur-schiwon.de>
2018-04-04 17:40:25 +0200

    fix class check for master?
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,12 @@
 <?php
 
-define('PHPUNIT_RUN', 1);
+if (!defined('PHPUNIT_RUN')) {
+	define('PHPUNIT_RUN', 1);
+}
 
 require_once __DIR__ . '/../../../lib/base.php';
 
-if (!class_exists('PHPUnit_Framework_TestCase')) {
+if (!class_exists('\PHPUnit\Framework\TestCase')) {
 	require_once('PHPUnit/Autoload.php');
 }
 
