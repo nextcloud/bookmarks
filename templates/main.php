1e580bc25fc2f16bb702a0f0b2556d54277a3a71
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-08-03 16:36:10 +0200

    Design bump
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- templates/main.php
+++ templates/main.php
@@ -8,7 +8,7 @@ style('bookmarks', 'select2');
 <div id="app-navigation">
 	<div id="add-bookmark-slot"></div>
 	<div id="navigation-slot"></div>
-	<h3><span class="icon-tag"></span> <%- t('bookmarks', 'Tags') %></h3>
+	<h3><span class="icon-tag"></span> <?php p($l->t('Tags')); ?></h3>
 	<div id="favorite-tags-slot"></div>
 	<div id="settings-slot"></div>
 </div>
