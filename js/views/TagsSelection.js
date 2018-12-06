45c6b842cd01d2c97e00fab1150a500e706f82f6
Marcel Klehr <mklehr@gmx.net>
2018-12-06 20:04:28 +0100

    UI: Fix tag selection: Don't drop focus after selecting one tag
    fixes #604

--- js/views/TagsSelection.js
+++ js/views/TagsSelection.js
@@ -16,7 +16,7 @@ export default Marionette.View.extend({
 	},
 	initialize: function(options) {
 		this.app = options.app;
-		this.selected = options.selected || new Tags;
+		this.selected = options.selected || new Tags();
 		this.selected.comparator = 'name';
 
 		this.listenTo(this.selected, 'add', this.onChangeByAlgo);
@@ -27,16 +27,17 @@ export default Marionette.View.extend({
 		if (this.$el.hasClass('select2-hidden-accessible')) {
 			this.$el.select2('destroy');
 		}
-		this.$el.select2({
-			placeholder: t('bookmarks', 'Set tags'),
-			width: '100%',
-			tags: true,
-			multiple: true,
-			tokenSeparators: [','],
-			data: this.app.tags.pluck('name').map(function(name) {
-				return {id: name, text: name};
+		this.$el
+			.select2({
+				placeholder: t('bookmarks', 'Set tags'),
+				width: '100%',
+				tags: true,
+				multiple: true,
+				tokenSeparators: [','],
+				data: this.app.tags.pluck('name').map(function(name) {
+					return { id: name, text: name };
+				})
 			})
-		})
 			.val(this.selected.pluck('name'))
 			.trigger('change');
 	},
@@ -44,16 +45,19 @@ export default Marionette.View.extend({
 		this.$el.select2('destroy');
 	},
 	onAddByUser: function(e) {
-		var tag = this.app.tags.get(e.params.data.text) ||
-			new Tag({name: e.params.data.text, count: 1});
+		var that = this;
+		var tag =
+			this.app.tags.get(e.params.data.text) ||
+			new Tag({ name: e.params.data.text, count: 1 });
 		this.selected.add(tag);
+		setTimeout(function() {
+			that.$el.select2('open');
+		}, 150);
 	},
 	onRemoveByUser: function(e) {
 		this.selected.remove(e.params.data.text);
 	},
 	onChangeByAlgo: function(e) {
-		this.$el
-			.val(this.selected.pluck('name'))
-			.trigger('change');
+		this.$el.val(this.selected.pluck('name')).trigger('change');
 	}
 });
