38e6f607a2878df99caee7de1bbd3a3d22fd3afc
Marcel Klehr <mklehr@gmx.net>
2019-03-25 12:38:53 +0100

    Fix typos
    fxies #674

--- templates/admin.php
+++ templates/admin.php
@@ -13,7 +13,7 @@ script('bookmarks', 'dist/admin.bundle');
             <?php print($l->t('You can either sign up for free at <a href="http://screeenly.com">screeenly.com</a> or <a href="https://github.com/stefanzweifel/screeenly">setup your own server</a>.')); ?>
         </p>
         <p>
-                <label for="bookmarks_previews_screenly_url"><?php p($l->t('Screenly API URL')); ?></label>
+                <label for="bookmarks_previews_screenly_url"><?php p($l->t('Screeenly API URL')); ?></label>
                 <input id="bookmarks_previews_screenly_url" name="bookmarks_previews_screenly_url"
                            type="text" style="width: 250px;" value="<?php p($_['previews.screenly.url']); ?>" />
                 <span class="error-status icon-error-color" style="display: inline-block"></span>
@@ -21,7 +21,7 @@ script('bookmarks', 'dist/admin.bundle');
 
         </p>
         <p>
-                <label for="bookmarks_previews_screenly_token"><?php p($l->t('Screenly API key')); ?></label>
+                <label for="bookmarks_previews_screenly_token"><?php p($l->t('Screeenly API key')); ?></label>
                 <input id="bookmarks_previews_screenly_token" name="bookmarks_previews_screenly_token"
                            type="text" style="width: 250px;" value="<?php p($_['previews.screenly.token']); ?>" />
                 <span class="error-status icon-error-color" style="display: inline-block"></span>
