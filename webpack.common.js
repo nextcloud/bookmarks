a2b8b45d9c2cff526d0f858dec7ff20eade1bbfb
Marcel Klehr <mklehr@gmx.net>
2019-12-12 16:35:56 +0100

    Fix browser compat
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- webpack.common.js
+++ webpack.common.js
@@ -42,7 +42,10 @@ module.exports = {
 				use: {
 					loader: 'babel-loader',
 					options: {
-						presets: ['@babel/preset-env']
+						presets: ['@babel/preset-env', {
+      targets: '> 0.25%, not dead',
+      useBuiltIns: "usage",
+    }]
 					}
 				}
 			}
