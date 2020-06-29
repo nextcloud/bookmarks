96e9d5ab8c8387a3697b6ce9fdcd674004adbe18
Marcel Klehr <mklehr@gmx.net>
2020-06-29 16:13:00 +0200

    v3.2.4

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,8 +2,8 @@
 
 const rev = '#1'
 
-const DYNAMIC_CACHE = 'dynamic-cache-v3.2.3' + rev
-const STATIC_CACHE = 'static-cache-v3.2.3' + rev
+const DYNAMIC_CACHE = 'dynamic-cache-v3.2.4' + rev
+const STATIC_CACHE = 'static-cache-v3.2.4' + rev
 const FILES_TO_CACHE = [
 	'./',
 ]
