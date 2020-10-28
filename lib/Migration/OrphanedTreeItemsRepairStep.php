937381017402e6f51cb6c96cae44c67fe7b66d2d
Marcel Klehr <mklehr@gmx.net>
2020-10-28 11:53:33 +0100

    Extend OrphanedTreeItems repair step
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>
    (cherry picked from commit ff910a0a2c8721ae29c61ba8832328ba5bfd84dc)

--- lib/Migration/OrphanedTreeItemsRepairStep.php
+++ lib/Migration/OrphanedTreeItemsRepairStep.php
@@ -43,7 +43,7 @@ class OrphanedTreeItemsRepairStep implements IRepairStep {
 				->execute();
 			$i++;
 		}
-		$output->info("Removed $i orphaned bookmarks");
+		$output->info("Removed $i orphaned bookmarks entries");
 
 		$qb = $this->db->getQueryBuilder();
 		$qb->select('t.id')
@@ -62,6 +62,57 @@ class OrphanedTreeItemsRepairStep implements IRepairStep {
 				->execute();
 			$i++;
 		}
+		$output->info("Removed $i orphaned folders entries");
+
+		$qb = $this->db->getQueryBuilder();
+		$qb->select('t.id', 't.type')
+			->from('bookmarks_tree', 't')
+			->leftJoin('t', 'bookmarks_folders', 'f', $qb->expr()->eq('t.parent_folder', 'f.id'))
+			->where($qb->expr()->isNull('f.id'));
+		$orphanedTreeItems = $qb->execute();
+		$i = 0;
+		while ($treeItem = $orphanedTreeItems->fetch()) {
+			$qb = $this->db->getQueryBuilder();
+			$qb->delete('bookmarks_tree')
+				->where($qb->expr()->eq('id', $qb->createPositionalParameter($treeItem['id'])))
+				->andWhere($qb->expr()->eq('type', $qb->createPositionalParameter($treeItem['type'])))
+				->execute();
+			$i++;
+		}
+		$output->info("Removed $i orphaned children entries");
+
+		$qb = $this->db->getQueryBuilder();
+		$qb->select('f.id')
+			->from('bookmarks_folders', 'f')
+			->leftJoin('f', 'bookmarks_tree', 't', $qb->expr()->eq('t.id', 'f.id'))
+			->leftJoin('f', 'bookmarks_root_folders', 'r', $qb->expr()->eq('f.id', 'r.folder_id'))
+			->where($qb->expr()->isNull('t.id'))
+			->andWhere($qb->expr()->isNull('r.folder_id'));
+		$orphanedFolders = $qb->execute();
+		$i = 0;
+		while ($folder = $orphanedFolders->fetchColumn()) {
+			$qb = $this->db->getQueryBuilder();
+			$qb->delete('bookmarks_folders')
+				->where($qb->expr()->eq('id', $qb->createPositionalParameter($folder)))
+				->execute();
+			$i++;
+		}
 		$output->info("Removed $i orphaned bookmark folders");
+
+		$qb = $this->db->getQueryBuilder();
+		$qb->select('b.id')
+			->from('bookmarks', 'b')
+			->leftJoin('b', 'bookmarks_tree', 't', $qb->expr()->eq('t.id', 'b.id'))
+			->where($qb->expr()->isNull('t.id'));
+		$orphanedBookmarks = $qb->execute();
+		$i = 0;
+		while ($bookmark = $orphanedBookmarks->fetchColumn()) {
+			$qb = $this->db->getQueryBuilder();
+			$qb->delete('bookmarks')
+				->where($qb->expr()->eq('id', $qb->createPositionalParameter($bookmark)))
+				->execute();
+			$i++;
+		}
+		$output->info("Removed $i orphaned bookmarks");
 	}
 }
