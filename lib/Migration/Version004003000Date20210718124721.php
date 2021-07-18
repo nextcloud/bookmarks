991e878dbb0c2ddcd6ec9d4b3ba1a53d40c62459
Marcel Klehr <mklehr@gmx.net>
2021-07-18 11:56:49 +0200

    Migration: Add clickcount indices
    fixes #1588
    
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

---
+++ lib/Migration/Version004003000Date20210718124721.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * Copyright (c) 2020. The Nextcloud Bookmarks contributors.
+ *
+ * This file is licensed under the Affero General Public License version 3 or later. See the COPYING file.
+ */
+
+namespace OCA\Bookmarks\Migration;
+
+use Closure;
+use Doctrine\DBAL\Schema\SchemaException;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version004003000Date20210718124721 extends SimpleMigrationStep {
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 *
+	 * @return void
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 *
+	 * @return ISchemaWrapper
+	 *
+	 * @throws SchemaException
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+		// Add clickcount indices
+		if ($schema->hasTable('bookmarks')) {
+			$table = $schema->getTable('bookmarks');
+			$table->addIndex(['clickcount'], 'bookmarks_clickcount');
+			$table->addIndex(['user_id', 'clickcount'], 'bookmarks_user_clickcount');
+		}
+
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 *
+	 * @return void
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+}
