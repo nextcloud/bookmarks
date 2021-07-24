7b2094b0b1ccae2d0f20d7e9d230032d345012c4
Marcel Klehr <mklehr@gmx.net>
2021-07-24 14:30:47 +0200

    Update webpack and babel config
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- webpack.js
+++ webpack.js
@@ -1,27 +1,9 @@
-const { merge } = require('webpack-merge')
 const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
-const config = {
-	entry: {
-		admin: path.join(__dirname, 'src', 'admin.js'),
-		'service-worker': path.join(__dirname, 'src', 'service-worker.js'),
-		flow: path.join(__dirname, 'src', 'flow.js'),
-		dashboard: path.join(__dirname, 'src', 'dashboard.js'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-			},
-		],
-	},
-}
+module.exports = webpackConfig
 
-const mergedConfigs = merge(config, webpackConfig)
-
-// Remove duplicate rules by the `test` key
-mergedConfigs.module.rules = mergedConfigs.module.rules.filter((v, i, a) => a.findIndex(t => (t.test.toString() === v.test.toString())) === i)
-
-module.exports = mergedConfigs
+webpackConfig.entry.admin = path.join(__dirname, 'src', 'admin.js')
+webpackConfig.entry['service-worker'] = path.join(__dirname, 'src', 'service-worker.js')
+webpackConfig.entry.flow = path.join(__dirname, 'src', 'flow.js')
+webpackConfig.entry.dashboard = path.join(__dirname, 'src', 'dashboard.js')
