bde34dcaa56a0e377f921d233efca68e85d510eb
Marcel Klehr <mklehr@gmx.net>
2022-11-18 15:13:12 +0100

    TestCase: Add support for nc 26
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -24,28 +24,29 @@
 
 namespace OCA\Bookmarks\Tests;
 
+use OCP\IDBConnection;
 use PHPUnit\Framework;
 
 class TestCase extends Framework\TestCase {
 	protected function cleanUp(): void {
 		parent::setUp();
-		$query = \OC_DB::prepare('DELETE FROM *PREFIX*bookmarks');
+		$query = \OC::$server->get(IDBConnection::class)->prepare('DELETE FROM *PREFIX*bookmarks');
 		$query->execute();
-		$query = \OC_DB::prepare('DELETE FROM *PREFIX*bookmarks_tags');
+		$query = \OC::$server->get(IDBConnection::class)->prepare('DELETE FROM *PREFIX*bookmarks_tags');
 		$query->execute();
-		$query = \OC_DB::prepare('DELETE FROM *PREFIX*bookmarks_folders');
+		$query = \OC::$server->get(IDBConnection::class)->prepare('DELETE FROM *PREFIX*bookmarks_folders');
 		$query->execute();
-		$query = \OC_DB::prepare('DELETE FROM *PREFIX*bookmarks_root_folders');
+		$query = \OC::$server->get(IDBConnection::class)->prepare('DELETE FROM *PREFIX*bookmarks_root_folders');
 		$query->execute();
-		$query = \OC_DB::prepare('DELETE FROM *PREFIX*bookmarks_folders_public');
+		$query = \OC::$server->get(IDBConnection::class)->prepare('DELETE FROM *PREFIX*bookmarks_folders_public');
 		$query->execute();
-		$query = \OC_DB::prepare('DELETE FROM *PREFIX*bookmarks_tree');
+		$query = \OC::$server->get(IDBConnection::class)->prepare('DELETE FROM *PREFIX*bookmarks_tree');
 		$query->execute();
-		$query = \OC_DB::prepare('DELETE FROM *PREFIX*bookmarks_shares');
+		$query = \OC::$server->get(IDBConnection::class)->prepare('DELETE FROM *PREFIX*bookmarks_shares');
 		$query->execute();
-		$query = \OC_DB::prepare('DELETE FROM *PREFIX*bookmarks_shared_folders');
+		$query = \OC::$server->get(IDBConnection::class)->prepare('DELETE FROM *PREFIX*bookmarks_shared_folders');
 		$query->execute();
-		$query = \OC_DB::prepare('DELETE FROM *PREFIX*bookmarks_shared_to_shares');
+		$query = \OC::$server->get(IDBConnection::class)->prepare('DELETE FROM *PREFIX*bookmarks_shared_to_shares');
 		$query->execute();
 	}
 }
