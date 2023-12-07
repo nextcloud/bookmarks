ca59d8038dfaac9d55b5ca364f56ec619d2af7d1
Marcel Klehr <mklehr@gmx.net>
2023-12-07 14:13:11 +0100

    fix(docs): use rtd theme
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,8 @@ release = u''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-'sphinxcontrib.httpdomain'
+'sphinxcontrib.httpdomain',
+'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -77,7 +78,7 @@ primary_domain = 'http'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
