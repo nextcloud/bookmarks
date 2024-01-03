c2213b9e09116eca7dd5344aeefb714b74ea1c77
Marcel Klehr <mklehr@gmx.net>
2024-01-03 13:56:52 +0100

    fix(BookmarkWithTagsAndParent): Avoid requerying tags if a bookmark has no tags
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/Db/BookmarkWithTagsAndParent.php
+++ lib/Db/BookmarkWithTagsAndParent.php
@@ -23,12 +23,18 @@ class BookmarkWithTagsAndParent extends Bookmark {
 	public function toArray(): array {
 		$array = [];
 		foreach (self::$fields as $field) {
-			if ($field === 'tags' && is_string($this->{$field})) {
-				$array[$field] = $this->{$field} === ''? [] : array_values(array_unique(explode(',', $this->{$field})));
-				continue;
+			if ($field === 'tags') {
+				if (is_string($this->{$field})) {
+					$array[$field] = $this->{$field} === '' ? [] : array_values(array_unique(explode(',', $this->{$field})));
+					continue;
+				}
+				if ($this->{$field} === null) {
+					$array[$field] = [];
+					continue;
+				}
 			}
 			if ($field === 'folders') {
-				if ($this->{$field} === '') {
+				if ($this->{$field} === '' || $this->{$field} === null) {
 					$array[$field] = [];
 				} else {
 					$array[$field] = array_values(array_unique(array_map(static function ($id) {
