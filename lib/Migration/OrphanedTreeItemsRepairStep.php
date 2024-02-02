fcc2029427b3c222fb4fff4e985842fdbb0dc24c
Marcel Klehr <mklehr@gmx.net>
2024-02-02 19:27:50 +0100

    fix(Copyright): Hello 2024
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/Migration/OrphanedTreeItemsRepairStep.php
+++ lib/Migration/OrphanedTreeItemsRepairStep.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (c) 2020. The Nextcloud Bookmarks contributors.
+ * Copyright (c) 2020-2024. The Nextcloud Bookmarks contributors.
  *
  * This file is licensed under the Affero General Public License version 3 or later. See the COPYING file.
  */
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <!--
-  - Copyright (c) 2020. The Nextcloud Bookmarks contributors.
+  - Copyright (c) 2020-2024. The Nextcloud Bookmarks contributors.
   -
   - This file is licensed under the Affero General Public License version 3 or later. See the COPYING file.
   -->
