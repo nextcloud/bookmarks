c2716ec66e18691613e771b7b15a1c76e363aad1
Marcel Klehr <mklehr@gmx.net>
2025-10-02 10:02:38 +0200

    fix: Fix paths in bootstrap.php
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,7 +14,7 @@ if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
 
-require_once __DIR__ . '/../../../../lib/base.php';
-require_once __DIR__ . '/../../../../tests/autoload.php';
+require_once __DIR__ . '/../../../lib/base.php';
+require_once __DIR__ . '/../../../tests/autoload.php';
 
 Server::get(IAppManager::class)->loadApp('bookmarks');
